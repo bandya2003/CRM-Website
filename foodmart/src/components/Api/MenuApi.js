68aa5555b6ff8dd2a8ef7a416977ff0e33f8b761
AsifStar786 <asifstar135@gmail.com>
2022-10-23 20:33:44 +0530

    Created FoodMart - Web-app for a Restaurant

---
+++ foodmart/src/components/Api/MenuApi.js
@@ -0,0 +1,164 @@
+const Menu = [
+    {
+        id:1,
+        image:"https://www.marketingmind.in/wp-content/uploads/2021/01/maggi.jpg",
+        name: "Maggie",
+        category: "BreakFast",
+        price:30,
+        description:"The best Maggie in market... Have the awesome taste in your breakfast."
+    },
+    {
+        id:2,
+        image:"https://www.indianhealthyrecipes.com/wp-content/uploads/2020/12/poori-puri-recipe-500x500.jpg",
+        name: "Poori",
+        category: "BreakFast",
+        price:35,
+        description:"The poori with chana masala will fill your stomach in the early morning... Have the experience"
+    },
+    {
+        id:3,
+        image:"https://www.vegrecipesofindia.com/wp-content/uploads/2021/07/dosa-recipe-3.jpg",
+        name: "Dosa",
+        category: "BreakFast",
+        price:50,
+        description:"The best indian Dosa is available here... Taste this awesome south indian dish."
+    },
+    {
+        id:4,
+        image:"https://pipingpotcurry.com/wp-content/uploads/2020/12/Poha-Recipe-indori-Piping-Pot-Curry.jpg",
+        name: "Poha",
+        category: "BreakFast",
+        price:25,
+        description:"This is a typpical indian breakfast best in taste... In cheapest price you have it."
+    },
+    {
+        id:5,
+        image:"https://static.toiimg.com/photo/53109843.cms",
+        name: "Aloo Paratha",
+        category: "BreakFast",
+        price:45,
+        description:"Homemade paratha is the most favourite item of almost everyone... The same taste of Paratha you get here."
+    },
+    {
+        id:6,
+        image:"https://deliverfood2me.in/wp-content/uploads/2018/03/Combo-South-Indian-NonVegMid.jpg",
+        name: "Veg Meal",
+        category: "Lunch",
+        price:40,
+        description:"As a meal the most satisfying and hygenic meal you'll get in least price..."
+    },
+    {
+        id:7,
+        image:"https://media.dinedelicious.in/wp-content/uploads/2021/07/Matar-Paneer-Pulao-2-500x500.jpg",
+        name: "Paneer rice",
+        category: "Lunch",
+        price:65,
+        description:"Here you get Rice with delicious Paneer, best in taste least in price... So come and have."
+    },
+    {
+        id:8,
+        image:"https://thespicemess.com/wp-content/uploads/2020/12/Chicken-Curry-12.jpg",
+        name: "Chicken Meal",
+        category: "Lunch",
+        price:75,
+        description:"When chicken comes with meal, that's special... So come and fill your hunger with tasty chicken."
+    },
+    {
+        id:9,
+        image:"https://10minutesrecipe.com/wp-content/uploads/2022/03/Indian-Chicken-Biryani-Recipe.jpg",
+        name: "Chicken biryani",
+        category: "Lunch",
+        price:105,
+        description:"Among moghol dishes Biryani is most famous. Here you get the most special and tasty biryani in right price..."
+    },
+    {
+        id:10,
+        image:"https://www.licious.in/blog/wp-content/uploads/2022/03/Licious-Holi-2022-013-min-1-1024x1024-1.jpg",
+        name: "Mutton Curry Rice",
+        category: "Lunch",
+        price:95,
+        description:"Here comes simple rice with the most tasty and delicious non-veg curry at only @95..."
+    },
+    {
+        id:11,
+        image:"https://ohthatsgood.com/wp-content/uploads/2022/08/Chicken-Lollipop-1200x1200-1.jpg",
+        name:"Chicken Lolipop",
+        category:"Evening",
+        price:"35",
+        description:"The best in market chicken lolipop is available here only @35"
+    },
+    {
+        id:12,
+        image:"https://img-global.cpcdn.com/recipes/1fc26dcfec280df4/680x482cq70/chicken-egg-roll-recipe-main-photo.jpg",
+        name:"Eggroll",
+        category:"Evening",
+        price:"30",
+        description:"Without an egg any dish is incomplete... So enjoy the rolled egg with burst of spice."
+    },
+    {
+        id:13,
+        image:"https://i.pinimg.com/736x/fa/c5/1f/fac51f5b3394a430e232c65319379840--nepal-food-networktrisha.jpg",
+        name:"Momo",
+        category:"Evening",
+        price:"40",
+        description:"The spice of our Momo will make your evening more charming and funfull.. Go for it !"
+    },
+    {
+        id:14,
+        image:"https://media-cdn.tripadvisor.com/media/photo-p/0e/94/07/a4/chicken-kabab.jpg",
+        name:"Chicken Kabab",
+        category:"Evening",
+        price:"50",
+        description:"Here comes the spicy and burned chicken, check it out and have fun with friends..."
+    },
+    {
+        id:15,
+        image:"https://www.whiskaffair.com/wp-content/uploads/2020/09/Veg-Cutlet-2-3.jpg",
+        name:"Veg/Egg Cutlet",
+        category:"Evening",
+        price:"20",
+        description:"Try the typpical evening snacks, cutlet in exclusive quality and price..."
+    },
+    {
+        id:16,
+        image:"https://www.archanaskitchen.com/images/archanaskitchen/0-Archanas-Kitchen-Recipes/2019/Preethi_Special_Sunday_Lunches_Keema_Bhindi_Punjabi_Toor_Dal_Tadka_Jowar_Atta_Roti_Indian_Sliced_Salad_1.jpg",
+        name:"Rooti-Tadhka",
+        category:"Dinner",
+        price:60,
+        description:"Finish simple your day with simple rooti-tadhka here, that's best in the market..."
+    },
+    {
+        id:17,
+        image:"https://www.cubesnjuliennes.com/wp-content/uploads/2021/03/Best-Mutton-Biryani-Recipe.jpg",
+        name:"Biryani-all",
+        category:"Dinner",
+        price:105-120,
+        description:"You can go with chicken/motton biryani as your dinner and kill your hunger at the end of the day..."
+    },
+    {
+        id:18,
+        image:"https://flybuy.in/wp-content/uploads/2020/06/kash.png",
+        name:"Pulao",
+        category:"Dinner",
+        price:70,
+        description:"Here's your favourite Pulao that taste just like your mother... Have the taste !"
+    },
+    {
+        id:19,
+        image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/oxlvv1lfoa9teb8kcquu",
+        name:"Rajma Chawal",
+        category:"Dinner",
+        price:75,
+        description:"Try this famous north Indian dish at cheapest price and experience the best taste..."
+    },
+    {
+        id:20,
+        image:"https://www.madhuseverydayindian.com/wp-content/uploads/2020/06/frozen-mixed-veggies-fry-indian-1.jpg",
+        name:"Mixed Veg Meal",
+        category:"Dinner",
+        price:65,
+        description:"Enjoy the spicy veg mixture and complete your dinner at cheap price and good nutrients..."
+    }
+]
+
+export default Menu
