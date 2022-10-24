2859d9e5ab776804a5b170336ed5a699f4fce1bf
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2022-10-24 20:59:20 +0530

    Netflix clone added

---
+++ netflix clone/src/components/index.js
@@ -0,0 +1,10 @@
+export { default as NavBar } from './NavBar'; // named export Shorthand
+export { default as Body } from './Body';
+export { default as Background } from './Background';
+export { default as Todo } from './Todos';
+
+/*
+   import Navbar from './NavBar';
+   export { Navbar };
+   Named Export
+*/
